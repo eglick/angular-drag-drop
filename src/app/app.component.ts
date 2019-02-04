ba544c5890f2a58a4f38559b41c6495b70f3f828
Ephraim Glick <eglick@prosper.com>
2019-02-03 20:55:53 -0500

    Create drag and drop demo using cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+interface Circle {
+  index: string;
+  id: string;
+}
+
+interface Square {
+  index: string;
+  id: string;
+  data: HTMLElement[];
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-drag-drop-demo';
+  title = 'Drag and Drop Demo';
+  
+  circles2: Circle[] = [];
+  circles = new Array(5).fill(null).map((el, idx) => `circ-${idx}`);
+  squares: Square[] = [];
+
+  constructor() {
+    for (let i = 0; i < 100; i++) {
+      this.squares.push({
+        index: `${i}`,
+        id: `sq-${i}`,
+        data: []
+      });
+    }
+    for (let i = 0; i < 5; i++) {
+      this.circles2.push({
+        index: `${i}`,
+        id: `circle-${i}`
+      });
+    }
+    // console.log('squares: ', this.squares);
+    // console.log('squareIds: ', this.squareIds);
+    // console.log('squareData: ', this.squareData);
+    // console.log('circleIds: ', this.circleIds);
+  }
+
+  get squareIds() {
+    return this.squares.map(sq => sq.id);
+  }
+
+  get squareData() {
+    return this.squares.map(sq => sq.data);
+  }
+
+  drop(event) {
+    const circleId = event.item.element.nativeElement.id;
+    const squareId = event.container.element.nativeElement.id;
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      console.log('moved', circleId, 'from index', event.previousIndex, 'to index', event.currentIndex);
+    } else {
+      if (!event.container.data) { return; }
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+      console.log('dropped', circleId, 'into', squareId);
+    }
+
+  }
+
 }
